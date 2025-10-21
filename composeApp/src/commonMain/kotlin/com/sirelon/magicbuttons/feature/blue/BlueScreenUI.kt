66f26a722effeee23f1bcbf82ca871680dc0a31d
sirelon <sasha.sirelon@gmail.com>
2025-10-21 13:03:02 +0300

    performance in animation

--- composeApp/src/commonMain/kotlin/com/sirelon/magicbuttons/feature/blue/BlueScreenUI.kt
+++ composeApp/src/commonMain/kotlin/com/sirelon/magicbuttons/feature/blue/BlueScreenUI.kt
@@ -1,8 +1,9 @@
 package com.sirelon.magicbuttons.feature.blue
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
@@ -18,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -53,6 +55,11 @@ private fun BlueScreen(onBack: () -> Unit) {
 
         val pressed by interaction.collectIsPressedAsState()
 
+        val bgColor by animateColorAsState(
+            targetValue = if (pressed) Color(0xD10D1A35) else Color(0xFF4A5B7C),
+            animationSpec = tween(durationMillis = 150, easing = EaseOut)
+        )
+
         Box(
             modifier = Modifier.fillMaxSize().padding(paddingValues),
             contentAlignment = Alignment.Center,
@@ -61,20 +68,14 @@ private fun BlueScreen(onBack: () -> Unit) {
             Image(
                 modifier = Modifier
                     .matchParentSize()
+                    .drawWithContent {
+                        drawContent()
+                        drawRect(color = bgColor)
+                    }
                     .blur(50.dp),
                 contentScale = ContentScale.Crop,
                 painter = painterResource(Res.drawable._6),
                 contentDescription = null,
-            )
-
-            val color by animateColorAsState(
-                targetValue = if (pressed) Color(0xD10D1A35) else Color(0xFF4A5B7C)
-            )
-
-            Box(
-                modifier = Modifier
-                    .matchParentSize()
-                    .background(color = color)
             )
 
             MagicBlueButton("Cancel", onClick = {}, interaction = interaction)
